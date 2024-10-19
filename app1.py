86edd2d6ff748ab530b0f09097c27fb997d99985
horoto9089 <horoto1005@gmail.com>
2024-10-19 12:41:31 +0900

    snow

--- app1.py
+++ app1.py
@@ -2,8 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.snow()
+
 st.header("卒論リポジトリ")
 st.subheader("専修大学4年 山原大翔")
 
 if st.button('Click Me'):
      st.write('こんにちは')
+
