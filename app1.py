c933c4ccd45fc3737bdb32805519e25598d39853
horoto9089 <horoto1005@gmail.com>
2024-10-19 14:30:26 +0900

    sxpander

--- app1.py
+++ app1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 st.snow()
 
@@ -12,3 +13,11 @@ st.subheader("専修大学4年 山原大翔")
 if st.button('Click Me'):
      st.write('こんにちは')
 
+with st.expander("st.expanderとは"):
+    st.write("このようにして、ユーザーがクリックすると、追加の情報を表示できるようになります。")
+
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+
+    plt.plot(x, y)
+    st.pyplot(plt)
